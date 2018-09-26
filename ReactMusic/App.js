1e648d6596f79b7733ff3dfdbc497eac3fc2daea
Unknown <opticalfalicy@gmail.com>
2018-09-26 18:05:18 -0500

    Completed up to tracks, need to finish adding them

--- ReactMusic/App.js
+++ ReactMusic/App.js
@@ -2,16 +2,28 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Header from './Header.js';
+import AlbumArt from './AlbumArt';
+import TrackDetails from './TrackDetails';
+import SeekBar from './SeekBar';
+import Controls from './Controls';
+
 
 export default class App extends React.Component {
   render() {
+    console.log(styles);
     return (
       <View style={styles.container}>
-        <Header message="Top of the POPS!" />
+        <Header message="Playing from Charts" />
+        <AlbumArt url="https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/CalculatingInfinity.jpg/220px-CalculatingInfinity.jpg" />
+        <TrackDetails title="43% Burnt"
+          artist="Dillinger Escape Plan" />
+        <SeekBar trackLength={431} currentPosition={259} />
+        <Controls />
       </View>
     );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
--- ReactMusic/TrackDetails.js
+++ ReactMusic/TrackDetails.js
@@ -1,3 +1,15 @@
+import React, { Component } from 'react';
+
+import {
+  View,
+  Text,
+  StyleSheet,
+  Image,
+  TouchableHighlight,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+
 const TrackDetails = ({
     title,
     artist,
@@ -9,7 +21,7 @@ const TrackDetails = ({
     <View style={styles.container}>
     <TouchableOpacity onPress={onAddPress}>
       <Image style={styles.button}
-        source={require('../img/ic_add_circle_outline_white.png')} />
+        source={require('./baseline_add_circle_outline_white_18dp.png')} />
     </TouchableOpacity>
     <View style={styles.detailsWrapper}>
       <Text style={styles.title} onPress={onTitlePress}>{title}</Text>
@@ -18,8 +30,53 @@ const TrackDetails = ({
     <TouchableOpacity onPress={onMorePress}>
       <View style={styles.moreButton}>
         <Image style={styles.moreButtonIcon}
-          source={require('../img/ic_more_horiz_white.png')} />
+          source={require('./baseline_more_horiz_white_18dp.png')} />
       </View>
     </TouchableOpacity>
   </View>
 );
+
+export default TrackDetails;
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 24,
+    flexDirection: 'row',
+    paddingLeft: 20,
+    alignItems: 'center',
+    paddingRight: 20,
+  },
+  detailsWrapper: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+  },
+  artist: {
+    color: 'rgba(255, 255, 255, 0.72)',
+    fontSize: 12,
+    marginTop: 4,
+  },
+  button: {
+    opacity: 0.72,
+  },
+  moreButton: {
+    borderColor: 'rgb(255, 255, 255)',
+    borderWidth: 2,
+    opacity: 0.72,
+    borderRadius: 10,
+    width: 20,
+    height: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  moreButtonIcon: {
+    height: 17,
+    width: 17,
+  }
+});
