4e0f48e058b37421c5ae43c7c5a2342683bfe7b8
Unknown <opticalfalicy@gmail.com>
2018-09-27 16:31:08 -0500

    Pausing work on this, moving on to metronome

--- ReactMusic/App.js
+++ ReactMusic/App.js
@@ -1,35 +1,29 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import Player from './Player';
 
-import Header from './Header.js';
-import AlbumArt from './AlbumArt';
-import TrackDetails from './TrackDetails';
-import SeekBar from './SeekBar';
-import Controls from './Controls';
+export const TRACKS = [
+  {
+    title: '43% Burnt',
+    artist: 'The Dillinger Escape Plan',
+    albumArtUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/CalculatingInfinity.jpg/220px-CalculatingInfinity.jpg",
+    audioUrl: "./02 43% Burnt.mp3",
+  },
+  {
+    title: 'The Running Board',
+    artist: 'The Dillinger Escape Plan',
+    albumArtUrl: "https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/CalculatingInfinity.jpg/220px-CalculatingInfinity.jpg",
+    audioUrl: './06 The Running Board.mp3',
+  },
+  {
+    title: 'Calculating Infinity',
+    artist: 'The Dillinger Escape Plan',
+    albumArtUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/CalculatingInfinity.jpg/220px-CalculatingInfinity.jpg',
+    audioUrl: './08 Calculating Infinity.mp3',
+  },
+];
 
-
-export default class App extends React.Component {
+export default class Tracks extends Component {
   render() {
-    console.log(styles);
-    return (
-      <View style={styles.container}>
-        <Header message="Playing from Charts" />
-        <AlbumArt url="https://upload.wikimedia.org/wikipedia/en/thumb/c/ce/CalculatingInfinity.jpg/220px-CalculatingInfinity.jpg" />
-        <TrackDetails title="43% Burnt"
-          artist="Dillinger Escape Plan" />
-        <SeekBar trackLength={431} currentPosition={259} />
-        <Controls />
-      </View>
-    );
+    return <Player tracks={TRACKS} />
   }
 }
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
